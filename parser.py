27b914b479e765f38f3bd1abb2544d2e167ad599
uor16x <u0r.home@gmail.com>
2022-05-18 15:17:38 +0300

    Initial image processing;

--- parser.py
+++ parser.py
@@ -6,6 +6,8 @@ import sys
 import getopt
 import base64
 
+BACKGROUPD_THRESHOLD = 30
+
 def read_image_arg():
   if len(sys.argv) != 2:
     print('Please pass base64 image as only argument')
@@ -17,8 +19,19 @@ def b64_to_cv2_img(b64img):
   return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
 
+def process_image(image):
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  blur = cv2.GaussianBlur(gray, (5,5),0)
+  image_width, img_height = numpy.shape(image)[:2]
+  background_threshold_level = gray[int(image_width/100)][int(image_width/2)]
+  threshold_level = background_threshold_level + BACKGROUPD_THRESHOLD
+  _, threshold_image = cv2.threshold(blur, threshold_level, 255, cv2.THRESH_BINARY)
+  return threshold_image
+
+
 def main():
     image = b64_to_cv2_img(read_image_arg())
+    processed_image = process_image(image)
     print(image)
 
 
